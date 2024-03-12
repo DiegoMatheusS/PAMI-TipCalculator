beeaa30d09f93073daf617ce60bf167783a48373
Diego Matheus <dms.diego.2019@gmail.com>
2024-03-12 19:18:27 -0300

    feat: criado botoes para calculos de porcentagem

--- TipCalculator/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TipCalculator.AssemblyInfo.cs
+++ TipCalculator/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TipCalculator.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TipCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+164a08668ec63d6ba87a3a5f965e8dd5af5dfae7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TipCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TipCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
